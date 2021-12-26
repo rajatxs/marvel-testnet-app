319b696ded310f30c3f886593a3441c200b9bc77
Rajat Sharma <rajat@developar.in>
2021-12-27 00:22:19 +0530

    Check prod

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 import express from 'express';
-import contract from "./contract.js";
-import path from "path";
-import { rootDir } from "./config.js";
-import { validate } from "./middlewares.js";
-import keyRoutes from "./routes/key.js";
+// import path from "path";
+// import { rootDir } from "./config.js";
+// import { validate } from "./middlewares.js";
+// import keyRoutes from "./routes/key.js";
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
-app.use("/key", keyRoutes);
+// app.use("/key", keyRoutes);
 
 app.get('/', (req, res) => {
-   res.sendFile(path.join(rootDir, "index.html"));
+   return res.json({
+      message: "It's work"
+   })
+   // res.sendFile(path.join(rootDir, "index.html"));
 });
 
-app.get('/balance', validate, (req, res) => {
-   let balance = 0; //contract.balanceOf();
+// app.get('/balance', validate, (req, res) => {
+//    let balance = 0; //contract.balanceOf();
 
-   return res.status(200).json({
-      value: balance
-   });
-});
+//    return res.status(200).json({
+//       value: balance
+//    });
+// });
 
 app.listen(port, () => {
    console.log("Server is running on port ", port);
