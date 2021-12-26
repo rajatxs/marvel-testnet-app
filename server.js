3b71f277d326fb6548892fd0f6adb6d7dff2a7d1
Rajat Sharma <rajat@developar.in>
2021-12-27 00:41:45 +0530

    Prod check

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import path from "path";
 import { rootDir } from "./config.js";
 import { validate } from "./middlewares.js";
-// import keyRoutes from "./routes/key.js";
+import keyRoutes from "./routes/key.js";
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
-// app.use("/key", keyRoutes);
+app.use("/key", keyRoutes);
 
 app.get('/', (req, res) => {
    res.sendFile(path.join(rootDir, "index.html"));
