2d2e05718163bd6d28125a6f0f8673f0eae5f2a6
Rajat Sharma <rajat@developar.in>
2021-12-27 00:57:57 +0530

    Prod check

--- utils/env.js
+++ utils/env.js
@@ -1,13 +1,24 @@
 
+// const env = {
+//    get ownerAddress() {
+//       return process.env.OWNER_ADDRESS;
+//    },
+//    get ownerPublicKey() {
+//       return process.env.OWNER_PUBLIC_KEY;
+//    },
+//    get ownerPrivateKey() {
+//       return process.env.OWNER_PRIVATE_KEY;
+//    }
+// }
 const env = {
    get ownerAddress() {
-      return process.env.OWNER_ADDRESS;
+      return "72eb0f073a4f8b60dedd6c9866c573c50a0403c4";
    },
    get ownerPublicKey() {
-      return process.env.OWNER_PUBLIC_KEY;
+      return "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDXcGnrBu6llt2DS1ZkSEc7G55q\nMeITWnLPywPscp2IDR1FtPK04Nh3qN6gqwkJgYsRfCeBtWuvZHF+W0RqtpWcHijv\nIHfSYwsO8PYJHyWZqh/TWf+ECMpiIGmsIbK2fYKeo7A553B+tT6JPm8EtgCLGh0L\nAYEPU1VHkihzA2uJYQIDAQAB\n-----END PUBLIC KEY-----\n";
    },
    get ownerPrivateKey() {
-      return process.env.OWNER_PRIVATE_KEY;
+      return "-----BEGIN PRIVATE KEY-----\nMIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBANdwaesG7qWW3YNL\nVmRIRzsbnmox4hNacs/LA+xynYgNHUW08rTg2Heo3qCrCQmBixF8J4G1a69kcX5b\nRGq2lZweKO8gd9JjCw7w9gkfJZmqH9NZ/4QIymIgaawhsrZ9gp6jsDnncH61Pok+\nbwS2AIsaHQsBgQ9TVUeSKHMDa4lhAgMBAAECgYEAsCE0RrQ9KN2FLZDLCWNZ8MXs\npi8v/9iDYsQ76fPuPEtUcie0ARKNho5lq/8IfWAOHPZeqhly90pryvy8m224MxEp\nB1HHIzjcBqalbq9Pg3E2Rqp1hqXiDA6JX33QXMRkh1+jivmSv1CGkFQsZDLAHmB2\nNp47FLtXbRtzB0GBmRECQQDr+YlsFZSZd+z54sJ9DcGZR5sGkxlAl8+PKTb7ar+L\nz4+Tq3hQBHI0GMMZr/PL7C+FzoOW9oSw9Nn4d5cyn/m9AkEA6bi/dwNXRLi0UQti\nyBPFAfjZeXXOK/38enQ1OVE8Ze/fzXsNM+Dz1tT0eOIifyf3S8sLBOBjTMJTt/Q5\nlzdedQJAOBy+cNUw4M2DrGgfl10ODETauFWCS7nNwV0rld1sbvKMUbEUivKDtZ+t\nxQ3yCyu+oTLaV6uHIg0XR94oNgGUgQJBAMbXEq1h/kDNEztQAH5Ps2/2Eq8u9nSe\n5MoZS56z6jqfVTwv0A+u9kRzcHnXiZm8mru0xm8jp9eKDEeYU9a+mHECQDDYQbkL\nG7HQ1MavBJf64ZUU5b2cUe2OlJMRJQBil5XHr3bFsmIgAp7ChCcI/8MAYI3oMA47\nxSvXv/of8LPJG+U=\n-----END PRIVATE KEY-----\n";
    }
 }
 
